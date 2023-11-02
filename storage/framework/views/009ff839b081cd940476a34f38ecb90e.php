fe8943cd4932c391aeb72d37248e67e16de106b2
Felipe Purgano <100305503+felipepurgano@users.noreply.github.com>
2023-11-02 00:33:25 -0300

    Add files via upload

--- storage/framework/views/009ff839b081cd940476a34f38ecb90e.php
+++ storage/framework/views/009ff839b081cd940476a34f38ecb90e.php
@@ -2,16 +2,65 @@
 require('base/base.html');
 ?>
 <!-- Código JavaScript do BootStrap para funcionar o dropdown da Navbar -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<link rel = 'stylesheet' href = 'css/styleoquee.css'>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+<link rel='stylesheet' href='css/styleoquee.css'>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>O que é HAS?</title>
 </head>
+
 <body>
-    <h1>O que é HAS?</h1>
+    <div class="titulo">
+        <h2>O que é HAS?</h2>
+    </div>
+    <div class="texto">
+        <p>O diagnóstico da HAS deve ser baseado em pelo menos duas aferições de PA por consulta, em pelo menos duas
+            consultas.</p>
+        <p>HAS crônica: condição clínica multifatorial, geralmente não associada a sintomas, caracterizada por elevação
+            sustentada dos níveis pressóricos sistólicos ≥ 140 mmHg e/ou diastólicos ≥ 90 mmHg.</p>
+        <p>Sempre que possível, o diagnóstico de HAS deve ser estabelecido em mais de uma visita médica. Geralmente de 2
+            a 3 visitas com intervalos de 1 a 4 semanas (dependendo do nível de pressão). O diagnóstico é feito em uma
+            única visita se a PA do paciente estiver maior ou igual a 180/110 mmHg e houver evidência de doença
+            cardiovascular.</p>
+        <p>Sempre que possível, incluir a medição da PA fora do consultório tanto para diagnóstico, quanto para
+            pacientes com PA elevada no consultório mesmo com tratamento otimizado.</p>
+        <br>
+            <p><h5>Crise hipertensiva:</h5>Pacientes com ou sem diagnóstico prévio de HAS podem apresentar episódios agudos,
+            geralmente com pressão arterial sistólica (PAS) superior a 180 mmHg e pressão arterial diastólica (PAD)
+            superior a 120 mmHg.</p>
+        <p>Podem ser sintomáticos dependendo do órgão primariamente acometido, estando o quadro clínico associado às
+            síndromes vasculares agudas (infarto do miocárdio, acidente vascular encefálico isquêmico ou hemorrágico e
+            síndromes aórticas) ou edema agudo de pulmão. Mais raramente associa-se a encefalopatia hipertensiva (com
+            alterações principalmente na fundoscopia e manifestações neurológicas) e hipertensão maligna (igualmente com
+            manifestações neurológicas e fundoscópicas, mas acompanhada de perda progressiva da função renal).</p>
+        <br>
+            <p><h5 class="atencao">Atenção: Nessas situações o tratamento deve ser com anti-hipertensivos de uso endovenoso.</h5></p>
+        <br>
+        <p><h5>Sinais e Sintomas dependem do órgão acometido</h5>
+            - Sinais neurológicos focais
+            <br>
+            - Alterações visuais
+            <br>
+            - Congestão (edema agudo de pulmão)
+            <br>
+            - Dor torácica
+            <br>
+            - Insuficiência renal
+            <br>
+            - Insuficiência hepática
+        </p>
+        <br>
+        <p><h5>Pseudocrise hipertensiva:</h5> aumento acentuado da PA mais frequentemente associado ao uso inadequado de
+            anti-hipertensivos, não apresentando lesão aguda de órgão alvo.</p>
+        <p>O tratamento anti-hipertensivo deve ser otimizado e as medidas não-farmacológicas reforçadas.</p>
+    </div>
 </body>
-</html><?php /**PATH C:\Users\Felipe\Desktop\PI\Novo\HAS\resources\views/o-que-e.blade.php ENDPATH**/ ?>
+
+</html>
+<?php /**PATH C:\Users\Felipe\Desktop\PI\Novo\HAS\resources\views/o-que-e.blade.php ENDPATH**/ ?>
--- storage/framework/views/f02174581812d25e5a1c463f9ca2b254.php
+++ storage/framework/views/f02174581812d25e5a1c463f9ca2b254.php
@@ -12,6 +12,57 @@ include( 'base/base.html' );
     <title>Cuidados - HAS</title>
 </head>
 <body>
-    <h1>Cuidados</h1>
+    <div class="titulo">
+        <h2>Cuidados com a HAS</h2>
+    </div>
+    <div class="texto">
+        <p>
+            <h5 class="prevencao">- Prevenção</h5>
+            Ao adotar uma dieta saudável com menor quantidade de sal, pacientes hipertensos devem consumir cerca 2 gramas de sal por dia, além de evitar alimentos processados e ultraprocessados, e praticar exercícios físicos regularmente. A recomendação é de 150 minutos de atividade física semanal. Também é importante gerenciar o estresse, abandonar o tabagismo e tomar medicamentos corretamente. Essas medidas são essenciais para baixar e controlar a pressão arterial.
+        </p>
+        <p>
+            <h5 class="tratamento">- Tratamento</h5>
+            A Atenção Primaria a Saúde, (APS) é a porta de entrada preferencial do Sistema Único de Saúde (SUS), responsável pelo cuidado longitudinal das pessoas com hipertensão, com a realização de ações preventivas e de promoção de saúde. Os pacientes são acompanhados pelas equipes multidisciplinares de Atenção Primária (eAP) e/ou da Estratégia Saúde da Família (eSF), que cuidam de pacientes hipertensos e diabéticos. 
+            <p>
+                Consultas regulares, conforme os protocolos de estratificação de risco e gravidade, são recomendações para o acompanhamento dos pacientes. A dispensação de medicamentos também é disponibilizada nas Unidades Básicas de Saúde (UBS). Portanto, o cidadão pode se dirigir à UBS mais próxima da residência dele e iniciar os cuidados com a saúde e o controle da hipertensão.
+            </p>
+            <p>
+                O Estado disponibiliza médicos especialistas e os exames complementares para o diagnóstico correto e suporte ao tratamento, além de assegurar o atendimento de retaguarda nos hospitais de urgência e emergência, preparados para atender os casos agudos e emergenciais.
+            </p>
+            <p>
+                A porta de entrada para os casos emergenciais são as Unidades de Pronto Atendimento (UPAs) de sua região e os prontos-socorros municipais, onde é feita a regulação conforme a doença e a gravidade, sendo o paciente referenciado ao Serviço Hospitalar mais adequado. 
+            </p>
+        </p>
+        <p>
+            <h5 class="alimentacao">- Dicas de alimentação</h5>
+            <p>
+                - Faça pelo menos três refeições (café da manhã, almoço e jantar) e dois lanches saudáveis por dia. Não pule as refeições;
+            </p>
+            <p>
+            - Coma diariamente, pelo menos três porções de legumes e verduras como parte das refeições e três porções ou mais de frutas nas sobremesas e lanches;
+            </p>
+            <p>
+                - Coma feijão com arroz todos os dias, ou, pelo menos, cinco vezes por semana que é uma combinação completa de proteínas e bom para a saúde;
+            </p>
+            <p>
+                - Consuma diariamente três porções de leite e derivados e uma porção de carnes, aves, peixes ou ovos. Retirar a gordura aparente das carnes e a pele das aves antes da preparação torna esses alimentos mais saudáveis; 
+            </p>
+            <p>
+                - Consuma, no máximo, uma porção por dia de óleos vegetais, azeite, manteiga ou margarina;
+            </p>
+            <p>
+                - Evite alimentos processados e ultraprocessados, tais como macarrão instantâneo, refrigerantes, embutidos (salame, presunto, mortadela etc), bolos, biscoitos doces e recheados, sobremesas e outras guloseimas como regra da alimentação. 
+            </p>
+            <p>
+                - Evite temperos industrializados e dê preferência aos temperos naturais;
+            </p>
+            <p>
+                - Diminua a quantidade de sal na comida e retire o saleiro da mesa, restringindo a 2 gramas de sal por dia;
+            </p>
+            <p>
+                - Beba pelo menos dois litros (6 a 8 copos) de água por dia. Dê preferência ao consumo de água nos intervalos das refeições;
+            </p>
+        </p>
+    </div>
 </body>
 </html><?php /**PATH C:\Users\Felipe\Desktop\PI\Novo\HAS\resources\views/cuidados.blade.php ENDPATH**/ ?>
