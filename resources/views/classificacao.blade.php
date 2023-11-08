272270bd1f1ae62721396d4783052d850507ac6a
Felipe Purgano <100305503+felipepurgano@users.noreply.github.com>
2023-11-07 22:36:45 -0300

    Nova atualização
    Feita a alteração da imagem do carrossel, alterados alguns elementos da página, como por exemplo na primeira "quebra" de cores com informações da HAS, adicionada uma nova página com as informações sobre a classificação, inserido também uma nova "quebra" na parte inferior mostrando mais uma maneira de entrar em contato além do botão "Contato" da navbar, também feita a inserção do footer na página inicial.

---
+++ resources/views/classificacao.blade.php
@@ -0,0 +1,28 @@
+<?php
+require('base/base.html');
+?>
+<!-- Código JavaScript do BootStrap para funcionar o dropdown da Navbar -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+<link rel='stylesheet' href='css/styleclassi.css'>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classificação da HAS</title>
+</head>
+<body>
+    <div class="titulo">
+        <h2>Classificação da HAS</h2>
+    </div>
+    <div class="texto">
+        <h3>Tabela de classificação da Hipertensão Arterial Sistêmica (HAS)</h3>
+        <p>
+            A HAS pode ser classificada como "Ótima", "Normal", "Pré-Hipertensão" ou "Hipertensão Arterial" nos estágios 1, 2 e 3. Veja na tabela quais os valores atualizados para cada uma delas, de acordo com as Diretrizes Brasileiras de Hipertensão Arterial 2020.
+        </p>
+        <img src="../img/tabela.png" alt="">
+    </div>
+</body>
+</html>
